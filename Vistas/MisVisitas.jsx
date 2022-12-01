8a998b4158e123dfd27d6daecd59c0712203b58f
jorsh08 <jorgeverdugo3005@gmail.com>
2022-12-01 00:33:09 -0800

    imagenes agregadas a la lista de mis visitas

--- Vistas/MisVisitas.jsx
+++ Vistas/MisVisitas.jsx
@@ -1,12 +1,7 @@
 import { StyleSheet, Text, ScrollView, View, TouchableOpacity, Image } from 'react-native'
 import React, { useEffect } from 'react'
-import imagen1 from '../assets/imagen6.png'
-const API = 'https://alexramval.pythonanywhere.com/mostrarPuntosTuristicos'
 
-const listaImagenes = {
-    imagen1: 'https://upload.wikimedia.org/wikipedia/commons/7/76/El_Palacio_Municipal_%28Atardecer%29.JPG',
-    imagen2: 'https://media-cdn.tripadvisor.com/media/photo-s/15/fd/7f/f2/mercado-municipal-cd.jpg'
-  }
+const API = 'https://alexramval.pythonanywhere.com/mostrarPuntosTuristicos'
 
 const MisVisitas = () => {
 
@@ -29,21 +24,19 @@ const MisVisitas = () => {
         
         <ScrollView>
             <View style={[styles.titulo, {flexDirection: 'row'}]}>
-
                 <View style={{flexDirection: 'row'}}>
-                    <Text>Mis visitas</Text>
+                    <Text style={{color:'#f1f1f1'}}>Mis visitas</Text>
                 </View>
-
-                <View style={{flexDirection: 'row'}}>
-                    <Text>asdad</Text>
-                </View>
-
             </View>
             <View style={[styles.listaPTuristicos]}>
                         
                 {listaVisitas.map(Visita => (
                     <TouchableOpacity key={Visita.id} style={[styles.tarjetaPTuristico, {flexDirection: 'column'}]}>
-                        <View style={[styles.contenedor, , {flexDirection: 'column'}]}>
+                        <Image
+                        source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/7/76/El_Palacio_Municipal_%28Atardecer%29.JPG'}}
+                        style={{width: '100%', height: '70%', borderTopLeftRadius: 15, borderTopRightRadius: 15}}
+                        />
+                        <View style={[styles.contenedor, {flexDirection: 'column'}]}>
                             <Text style={[{fontStyle: 'italic', fontWeight: 'bold'}, {flexDirection: 'column'}]}>{Visita.puntoNombre}</Text>
                             <Text style={[{fontStyle: 'italic', fontSize: 12}, {flexDirection: 'column'}]}>{Visita.puntoDir}</Text>
                         </View>
@@ -66,7 +59,7 @@ const styles = StyleSheet.create({
         marginTop: '5%',
         backgroundColor: '#3a9a3c',
         width: '80%',
-        height: 150,
+        height: 200,
         borderRadius: 15
     },
     listaPTuristicos: {
@@ -74,11 +67,10 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     contenedor: {
-        marginTop: '30%',
         marginLeft: '15%',
         marginRight: '15%',
+        marginTop: 5,
         width: '70%',
-        borderWidth: 1
     }
 })
 
